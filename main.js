91737667612f7afe73bd126d2cea49fcfb348696
Benjamin Zaporzan <benzaporzan@gmail.com>
2015-05-06 18:40:10 -0400

    code cleanup

--- main.js
+++ main.js
@@ -45,58 +45,46 @@ function addModconsole() {
 	      });
 }  
 
-function videoclick()
-{
-    socket.on("channelOpts", function()
-	      {
-		  if (CLIENT.rank >= 2) 
-		  {    
-                      $("#queue").css("pointer-events","visible");   
-		  }
-		  else 
-		  { 
-		      $("#queue").css("pointer-events","none");             
-		  }
-
-		  $("#videowrap").css("pointer-events","auto");      
-		  
-	      });
+function videoclick() {
+    socket.on("channelOpts", function() {
+	if (CLIENT.rank >= 2) {    
+            $("#queue").css("pointer-events","visible");   
+	}
+	else { 
+	    $("#queue").css("pointer-events","none");             
+	}
+	
+	$("#videowrap").css("pointer-events","auto");      
+    });
 } 
 
 videoclick();
 
 HideShadowMute();
-function HideShadowMute()
-{
-    socket.on("channelOpts", function()
-	      {
-		  if (CLIENT.rank >= 2) 
-		  {    
-                      $(".glyphicon:empty").css("pointer-events","auto");   
-		  }
-		  else 
-		  { 
-		      $(".glyphicon:empty").css("pointer-events","none");               
-		  }
-
-		  $("#videowrap").css("pointer-events","auto");         
-	      });
-} 
+function HideShadowMute() {
+    socket.on("channelOpts", function() {
+	if (CLIENT.rank >= 2) {    
+            $(".glyphicon:empty").css("pointer-events","auto");   
+	}
+	else { 
+	    $(".glyphicon:empty").css("pointer-events","none");               
+	}
+	
+	$("#videowrap").css("pointer-events","auto");         
+    });
+}
 
 addTriviaToggle();
-
 function addTriviaToggle() {
-    if( $('#trivtog').length == 0 ) 
-    {
+    if ($('#trivtog').length == 0 ) {
         var objTo = document.getElementById('leftcontrols')
         var modspan = document.createElement("span");
         modspan.innerHTML = "<button id='trivtog' class='btn btn-sm btn-default'>  Trivia/Jumble: On</button>"
         objTo.appendChild(modspan);
     }
-}  
+}
 
-
-$("#trivtog").click((function(){
+$("#trivtog").click((function() {
     var interId = null;
     var $ul = $("#timet ul");
     return function(e) {
@@ -105,7 +93,8 @@ $("#trivtog").click((function(){
             clearInterval(interId);
             interId = null;
 	    $(".hiddenbotchat").attr('class', 'shownbotchat');
-        } else {
+        }
+	else {
             $(this).text("Trivia/Jumble: Off");
             interId = setInterval(function() {
 		$('.botchat').parent().parent('div').attr('class', 'hiddenbotchat');
@@ -116,38 +105,37 @@ $("#trivtog").click((function(){
 
 drinkflair();
 function drinkflair(){
-    socket.on("drinkCount", function() 
-	      {
-		  setTimeout(
-		      function() 
-		      {
-			  $('#drinkbar h1 span').remove();
-			  lelcat = $('#drinkbar h1').text().replace('drinks','');
-			  $('#drinkbar h1').text(lelcat);
-			  lelcat = $('#drinkbar h1').text().replace('drink','');
-			  $('#drinkbar h1').text(lelcat);
-			  $('#drinkbar h1').append($('.drink span').last().clone());
-			  $('#drinkbar h1 span').append("<img src='http://i.imgur.com/gFQutcV.gif' />");
-		      }, 2000);
-
-	      });
+    socket.on("drinkCount", function() {
+	setTimeout(function() {
+	    $('#drinkbar h1 span').remove();
+	    lelcat = $('#drinkbar h1').text().replace('drinks','');
+	    $('#drinkbar h1').text(lelcat);
+	    lelcat = $('#drinkbar h1').text().replace('drink','');
+	    $('#drinkbar h1').text(lelcat);
+	    $('#drinkbar h1').append($('.drink span').last().clone());
+	    $('#drinkbar h1 span').append("<img src='http://i.imgur.com/gFQutcV.gif' />");
+	}, 2000);
+    });
 }
+
+
 if (location.host == "synchtube.me" || location.host == "www.synchtube.me") {
     location.href = location.protocol + "//cytu.be" + location.pathname;
 }
+
+
 removemiddlescreen();
 function removemiddlescreen(){
-    socket.on("chatMsg",function()
-	      {
-		  if( $('.removemiddlescreen').length > 0 ){$('.middlescreen, .removemiddlescreen').remove();}
-	      }
-	     );
+    socket.on("chatMsg", function() {
+	if ($('.removemiddlescreen').length > 0) {
+	    $('.middlescreen, .removemiddlescreen').remove();
+	}
+    });
 }
 
-function sucko(){
+function sucko() {
     var fek = $('.b3g3:last').text();
-    if (fek.length > 1)
-    {
+    if (fek.length > 1) {
 	fek2 = "url('" + fek +"')";
     }
     else {
@@ -158,41 +146,35 @@ function sucko(){
 
 
 hoverSound();
-function hoverSound()
-{
-    socket.on("chatMsg", function(args) 
-	      {
-		  var sucko = $('.hoversound7').last().text();
-		  var soundlink = 'http://' + sucko;
-		  $('.hoversound7').last().parent().append('<audio id="gifaudio"><source src="'+soundlink+'" type="audio/mpeg">');
+function hoverSound() {
+    socket.on("chatMsg", function(args) {
+	var sucko = $('.hoversound7').last().text();
+	var soundlink = 'http://' + sucko;
+	$('.hoversound7').last().parent().append('<audio id="gifaudio"><source src="'+soundlink+'" type="audio/mpeg">');
 
-		  $('#gifaudio').parent().append('<img src="http://i.imgur.com/4XgmiHM.png" height="20px"/>');
-		  $('#gifaudio').parent().on('click', function() {
-		      var boobs = $(this).find('audio');
-		      boobs[0].volume=.4;
-		      boobs[0].play();
-		  });
-		  $('.hoversound7').remove();
-		  $('#gifaudio').attr('id','giffedaudio');
+	$('#gifaudio').parent().append('<img src="http://i.imgur.com/4XgmiHM.png" height="20px"/>');
+	$('#gifaudio').parent().on('click', function() {
+	    var boobs = $(this).find('audio');
+	    boobs[0].volume=.4;
+	    boobs[0].play();
+	});
+	$('.hoversound7').remove();
+	$('#gifaudio').attr('id','giffedaudio');
 
-	      }
-	     )};
+    });
+}
 
 appendNotifybar();
 
-function appendNotifybar()
-{
+function appendNotifybar() {
 
-    if( $('#notifybar').length == 0 )
-    {
+    if( $('#notifybar').length == 0 ) {
 	$('#chatheader').append('<span id="notifybar"></span>');
     }
 
-    if( $('#notifymarquee').length == 0 )
-    {
+    if( $('#notifymarquee').length == 0 ) {
 	$('#notifybar').append('<marquee id="notifymarquee" behavior="scroll" direction="left"></marquee>');
     }
-
 }
 
 function webmVideo() {
@@ -200,14 +182,17 @@ function webmVideo() {
     $('span.vid').html('<video src="'+widgetHTML+'" controls></video>');
     $('span.vid').addClass('convertedvid').removeClass('vid');
 }
+
 $('#newpollbtn').click(function () {
     $("#pollwrap .checkbox input[type=checkbox]").prop("checked", true);
 });
 
-$(document).ready(function(){
+$(document).ready(function() {
     videoclick();
-    if( $('#stopaudiobutton').length != 0 ){return;}
-    else{
+    if ($('#stopaudiobutton').length != 0){
+	return;
+    }
+    else {
 	$('#leftcontrols').prepend('<span class="btn btn-sm btn-default" style="float: right;" id="stopaudiobutton">Stop Audio</span>');
 	$( "#stopaudiobutton" ).click(function() {
 	    var audioElements = document.getElementsByTagName('audio');
@@ -219,7 +204,9 @@ $(document).ready(function(){
 });
 
 $( ".motdclick" ).click(function() {
-    $( "#motdframe" ).attr( 'src', function ( i, val ) { return val; });
+    $("#motdframe").attr('src', function (i, val) {
+	return val;
+    });
 }); 
 
 
@@ -227,13 +214,19 @@ $( ".motdclick" ).click(function() {
 
 //apply username classes to usernames, hook this to chatMsg so it refreshes
 classApplier();
-function classApplier(){
-    $('.userlist_item span:nth-child(2)').each(function(){$(this).addClass($(this).text())});
-    $('.username').each(function(){$(this).addClass($(this).text().slice(0,-2))})
+function classApplier() {
+    $('.userlist_item span:nth-child(2)').each(function() {
+	$(this).addClass($(this).text())
+    });
+    $('.username').each(function() {
+	$(this).addClass($(this).text().slice(0,-2))
+    })
 	}
 
-function b3g3(){$('#messagebuffer').css("background-image","url("+$('.b3g3:last').text()+")");}
 b3g3();
+function b3g3(){
+    $('#messagebuffer').css("background-image","url("+$('.b3g3:last').text()+")");
+}
 
 //hook into the chatMsg frame
 socket.on("chatMsg", function() {
@@ -241,22 +234,35 @@ socket.on("chatMsg", function() {
     b3g3();
     speakz2();
     addsptoggle();
-    $('#boatskip').click(function(){socket.emit("voteskip")});
-    $('#memers3').click(function(){socket.emit("chatMsg",{"msg":":dankredhat:"});$('#memers3').remove();});
+    $('#boatskip').click(function() {
+	socket.emit("voteskip")
+    });
+    $('#memers3').click(function() {
+	socket.emit("chatMsg", {"msg":":dankredhat:"});
+	$('#memers3').remove();
+    });
 });
 
-function speakz2()
-{
-    if ( !$('.speakz2').length ){return;}
-    if ( !$('.spenabled2').length ) {return;}
+function speakz2() {
+    if (!$('.speakz2').length){
+	return;
+    }
+    if (!$('.spenabled2').length) {
+	return;
+    }
+
     var moobs=new Audio("http://webanywhere.cs.washington.edu/cgi-bin/espeak/getsound.pl?lang=english&text="+encodeURI($(".speakz2").last().text()));
     moobs.volume=.4;
     moobs.play();
     $(".speakz2").removeClass();
 }
 
-function addsptoggle(){
-    if ( $('#sptoggle2').length ){return;}
+function addsptoggle() {
+    if ($('#sptoggle2').length) {
+	return;
+    }
     $('#leftcontrols').prepend('<span class="btn btn-sm btn-default" style="float: right;" id="sptoggle2">ENABLE SPEAK</span>');
-    $( "#sptoggle2" ).click(function() {$(this).toggleClass("spenabled2");});
+    $( "#sptoggle2" ).click(function() {
+	$(this).toggleClass("spenabled2");
+    });
 }
