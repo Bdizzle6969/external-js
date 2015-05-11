914cbbc65eb8f514a336f1e1d85085e80e3815ce
Benjamin Zaporzan <benzaporzan@gmail.com>
2015-05-11 00:21:38 -0400

    started work on fixing drinkflair, too tired...

--- out/fmoyt-external.js
+++ out/fmoyt-external.js
@@ -129,28 +129,44 @@ module.exports = {
 };
 
 },{}],5:[function(require,module,exports){
+/*
+  Drink Flair and chatMsg fixes
+ */
 
-var __drink_timeout_handler = null;
+var __drinkCount = 0;
 
-drinkflair();
+socket.on("drinkCount", function(count) {
+    __drinkCount = count;
+    /*
+      $('#drinkbar h1 span').remove();
+      lelcat = $('#drinkbar h1').text().replace('drinks','');
+      $('#drinkbar h1').text(lelcat);
+      lelcat = $('#drinkbar h1').text().replace('drink','');
+      $('#drinkbar h1').text(lelcat);
+      $('#drinkbar h1').append($('.drink span').last().clone());
+      $('#drinkbar h1 span').append("<img src='http://i.imgur.com/gFQutcV.gif' />");
+    */
+});
 
-function drinkflair(){
-    socket.on("drinkCount", function() {
-	clearTimeout(__drink_timeout_handler);
-	__drink_timeout_handler = setTimeout(function() {
-	    $('#drinkbar h1 span').remove();
-	    lelcat = $('#drinkbar h1').text().replace('drinks','');
-	    $('#drinkbar h1').text(lelcat);
-	    lelcat = $('#drinkbar h1').text().replace('drink','');
-	    $('#drinkbar h1').text(lelcat);
-	    $('#drinkbar h1').append($('.drink span').last().clone());
-	    $('#drinkbar h1 span').append("<img src='http://i.imgur.com/gFQutcV.gif' />");
-	}, 2000);
-    });
+var handler = function(data) {
+    var div = data.div;
+    if ($(div).hasClass("drink")) {
+	//correct the inner text
+	var msg = "D" + data.msg;
+	
+	var spanText = div.querySelectorAll("span")[2];
+	spanText.innerHTML = msg;
+
+	//apply to header
+	var drinkBar = document.querySelector("#drinkbar h1");
+	console.log("drinkbar", drinkBar);
+	
+    }
 }
 
+
 module.exports = {
-    
+    handler: handler,
 };
 
 },{}],6:[function(require,module,exports){
