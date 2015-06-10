5773bd37d00c55c38d19cb46652aee246e324e36
Bdizzle6969 <Bdizzle6969@users.noreply.github.com>
2015-06-09 20:20:10 -0400

    Update trivia-toggle.js
    dont spawn trivia toggle button twice, need to re-gulp this when im less lazy

--- libs/ui/trivia-toggle.js
+++ libs/ui/trivia-toggle.js
@@ -4,11 +4,12 @@
 
 HideShadowMute();
 function HideShadowMute() {
+	if($('#trivtog').length == 0){
     var objTo = document.getElementById('leftcontrols')
     var modspan = document.createElement("span");
     modspan.innerHTML = "<button id='trivtog' class='btn btn-sm btn-default'>  Trivia/Jumble: On</button>"
     objTo.appendChild(modspan);
-}
+}}
 
 $("#trivtog").click((function() {
     var interId = null;
